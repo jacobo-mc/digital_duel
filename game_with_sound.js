882379036478deae41018bd3db63fbc5ae7fe678
Jacobo <77410525+jacobo-mc@users.noreply.github.com>
2022-02-07 19:37:30 -0500

    Update game_with_sound.js

--- game_with_sound.js
+++ game_with_sound.js
@@ -1,87 +1,14 @@
-function sound(src) {
-  this.sound = document.createElement("audio");
-  this.sound.src = src;
-  this.sound.setAttribute("preload", "auto");
-  this.sound.setAttribute("controls", "none");
-  this.sound.style.display = "none";
-  document.body.appendChild(this.sound);
-  this.play = function(){
-    this.sound.play();
-  }
-  this.stop = function(){
-    this.sound.pause();
-  }
-}
 backgroundMusic=new sound('music.wav')
 backgroundMusic.sound.setAttribute('loop',true)
 backgroundMusic.play()
 document.write("<p id=\"p1score\">P1: 0</p><BR/><p id=\"p2score\">P2: 0<\/p><canvas width=\"500\" height=\"400\"id=\"canvas\"></canvas>");
 var game_stop=false
-var Key = {
-  _pressed: {},
-  LEFT: 37,
-  UP: 38,
-  RIGHT: 39,
-  DOWN: 40,
-	W: 87,
-	A: 65,
-	S: 83,
-	D: 68,
-	SPACE: 32,
-	F: 70,
-  isDown: function(keyCode) {
-    return this._pressed[keyCode];
-  },
-  onKeydown: function(event) {
-    this._pressed[event.keyCode] = true;
-  },
-  onKeyup: function(event) {
-    delete this._pressed[event.keyCode];
-  }
-};
-window.addEventListener('keyup', function(event) { Key.onKeyup(event); }, false);
-window.addEventListener('keydown', function(event) { Key.onKeydown(event); }, false);
 const canv=document.getElementById("canvas");
 const ctx=canv.getContext('2d');
 var playerRect1={x:300,y:200,width:10,height:10}
 var playerRect2={x:200,y:300,width:10,height:10}
 var player1Score=0
 var player2Score=0
-function detectRectCollision(rect1,rect2){
-	if (rect1.x < rect2.x + rect2.width &&
-   rect1.x + rect1.width > rect2.x &&
-   rect1.y < rect2.y + rect2.height &&
-   rect1.y + rect1.height > rect2.y) {
-    return true;
- }else{
-	 return false;
- };
-};
-function detectCircleCollision(circle1,circle2){
-	var dx = circle1.x - circle2.x;
-	var dy = circle1.y - circle2.y;
-	var distance = Math.sqrt(dx * dx + dy * dy);
-
-	if (distance < circle1.radius + circle2.radius) {
-    return true;
-	}else{
-		return false;
-	};
-};
-function detectRectCircleCollision(circle,rect){
-    var distX = Math.abs(circle.x - rect.x-rect.width/2);
-    var distY = Math.abs(circle.y - rect.y-rect.height/2);
-
-    if (distX > (rect.width/2 + circle.radius)) { return false; }
-    if (distY > (rect.height/2 + circle.radius)) { return false; }
-
-    if (distX <= (rect.width/2)) { return true; } 
-    if (distY <= (rect.height/2)) { return true; }
-
-    var dx=distX-rect.width/2;
-    var dy=distY-rect.height/2;
-    return (dx*dx+dy*dy<=(circle.radius*circle.radius));
-}
 var missles=[]
 var shootDir1=1
 var shootDir2=1
